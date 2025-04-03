6b40c13fb881cd4017604cdad4651a7ff81bce42
Jaireua <jairenriquez30@gmail.com>
2025-04-02 22:00:21 -0500

    creating signup

--- src/signup.php
+++ src/signup.php
@@ -2,16 +2,20 @@
 
 include 'config/database.php';
 
+// Get form data from POST request
 $firstname = $_POST['firstname'];
 $lastname = $_POST['lastname'];
 $email = $_POST['email'];
 $password = $_POST['password'];
 
+// Prepare SQL insert statement for users table
 $sql = "INSERT INTO users (firstname, lastname, email, password) 
 VALUES ('$firstname', '$lastname', '$email', '$password')";
 
+// Execute SQL statement
 $answer = pg_query($conn, $sql);
 
+// Check if user was created successfully
 if ($answer) {
     echo "User created successfully";
 } else {
